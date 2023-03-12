bbb8da4704f756806435039f48ef2dc83b75a88c
klm127 <karlmiller127@gmail.com>
2023-03-12 15:21:23 -0400

    2nd migration

---
+++ database/migration/000002_init_schema.down.sql
@@ -0,0 +1,7 @@
+DELETE FROM "VoteRecords";
+DELETE FROM "Comments";
+DELETE FROM "Paths";
+DELETE FROM "Domains";
+DELETE FROM "AdminAssignments";
+DELETE FROM "GlobalModeratorAssignments";
+DELETE FROM "Users";
